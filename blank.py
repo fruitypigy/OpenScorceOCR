b26ae9d77e1d99ced1d022c26e9647540e6d99e5
fruitypigy <fruitypigy@gmail.com>
2021-06-02 14:19:10 -0400

    Referances now use 60x100 instead of being stupid

--- blank.py
+++ blank.py
@@ -1,26 +1,10 @@
+import ImageProcess as ip
 import cv2
-import PySimpleGUI as sg
-from SelectedViewer import SelectedViewer as sv
-from SelectedArea import SelectedArea as sa
 
-graph_element = sg.Graph((750, 750), (0, 750), (750, 0), 
-                key='graph', enable_events=True, background_color='white')
-
-layout = [[graph_element]]
-
-window = sg.Window('Selected Viewer Test', layout, finalize=True)
-
-frame = cv2.imread('PreviewTest.png')
-
-viewer = sv()
-
-graph = window['graph'] # type: sg.Graph
-
-while True:
-    event, values = window.read()
-
-    graph = viewer.drawSelected(graph, (6,6), 32)
-
-    if event == None:
-        exit()
-
+images = ('Segments\SegBM.jpg', 'Segments\SegBR.jpg', 'Segments\SegBL.jpg',
+        'Segments\SegM.jpg', 'Segments\SegTM.jpg',
+        'Segments\SegTR.jpg', 'Segments\SegTL.jpg')
+for x in range(len(images)):
+    img = cv2.imread(images[x])
+    resized = ip.resize(img, (60, 100))
+    cv2.imwrite(images[x], resized)

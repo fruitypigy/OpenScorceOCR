9c3b1a3b5508474335c3a411d3e5a1a20d9ce92b
fruitypigy <fruitypigy@gmail.com>
2021-06-14 11:29:43 -0400

    Switched to adjusting individual points with wasd

--- FilterSetup.py
+++ FilterSetup.py
@@ -24,30 +24,31 @@ def filterSetup(feed: Feed):
     input_elements = [sg.Text('Width'),
                       sg.Spin((list(range(100, 800))), 600, size=(6, 4), key='width', enable_events=True),
                       sg.Text('Height'),
-                      sg.Spin((list(range(100, 800))), 300, size=(6, 4), key='height', enable_events=True),
-                      sg.Text('Horizontal Stretch'),
-                      sg.Spin((list(range(-20, 20))), 0, size=(4, 4), key='hor_stretch', enable_events=True),
-                      sg.Text('Vertical Stretch'),
-                      sg.Spin((list(range(-20, 20))), 0, size=(4, 4), key='vert_stretch', enable_events=True),
-                      sg.Text('Horizontal Stretch 2'),
-                      sg.Spin((list(range(-20, 20))), 0, size=(4, 4), key='hor2_stretch', enable_events=True),
-                      sg.Text('Vertical Stretch 2'),
-                      sg.Spin((list(range(-20, 20))), 0, size=(4, 4), key='vert2_stretch', enable_events=True)]
+                      sg.Spin((list(range(100, 800))), 300, size=(6, 4), key='height', enable_events=True)]
 
     spin_element = sg.Text('Number of Digits'), sg.Spin([i for i in range(1, 19)], 1, size=(3, 2), key='segment_number')
+
+    point_selector = [sg.Radio('Point 1', 'point_sel', key='point1', default=True),
+                      sg.Radio('Point 2', 'point_sel', key='point2'),
+                      sg.Radio('Point 3', 'point_sel', key='point3'),
+                      sg.Radio('Point 4', 'point_sel', key='point4')]
 
     input_graph_element = sg.Graph(canvas_size=(feed.width, feed.height), graph_bottom_left=(0, feed.height),
                                    graph_top_right=(feed.width, 0), enable_events=True, drag_submits=True,
                                    key="graph", background_color='green')
 
     preview_element = [sg.Text('Preview')], [
-        sg.Graph((600, 300), (0, 300), (600, 0), background_color='grey', key='preview')]
+        sg.Graph((600, 400), (0, 400), (600, 0), background_color='grey', key='preview')]
 
-    setup_layout = [[input_graph_element, sg.Column(slider_elements), sg.Column(preview_element)], input_elements,
-                    [sg.Button('Confirm'), sg.Button('Quit'), spin_element]]
+    setup_layout = [[input_graph_element,
+                     sg.Column(slider_elements),
+                     sg.Column(preview_element)],
+                    point_selector, input_elements,
+                    [sg.Button('Confirm'),
+                     sg.Button('Quit'), spin_element]]
     # setup_layout = [[sg.Column(slider_elements), sg.Column(preview_element)], [sg.Button('Confirm'), sg.Button('Quit'), spin_element]]
 
-    setup_window = sg.Window('Setup', setup_layout)
+    setup_window = sg.Window('Setup', setup_layout, return_keyboard_events=True)
 
     graph = setup_window['graph']  # type: sg.Graph
     preview = setup_window['preview']  # type: sg.Graph
@@ -114,15 +115,36 @@ def filterSetup(feed: Feed):
             warped_encoded = None
             skip_warp = False
         elif len(coords) == 4 and ((not skip_warp) or
-                                   (event == 'width' or event == 'height') or event.endswith('stretch')):
-            print(f'Coords: {coords}')
-            adj_coords[0] = (coords[0][0] + int(values['hor_stretch'])), (coords[0][1] + int(values['vert_stretch']))
-            adj_coords[1] = (coords[1][0] + int(values['hor_stretch'])), (coords[1][1] + int(values['vert2_stretch']))
-            adj_coords[2] = (coords[2][0] + int(values['hor2_stretch']), coords[2][1] + int(values['vert_stretch']))
-            adj_coords[3] = (coords[3][0] + int(values['hor2_stretch']), coords[3][1] + int(values['vert2_stretch']))
-            print(f'Adjusted: {adj_coords}')
-            warped, warped_encoded = warpPerspective(feed.getFrame(True)[0], adj_coords, dims=(width, height))
+                                   (event == 'width' or
+                                    event == 'height') or
+                                   event in ['w', 'a', 's', 'd']):
+
+            if event in ['w', 'a', 's', 'd']:
+                point_count = 0
+                for point in ['point1', 'point2', 'point3', 'point4']:
+                    if values[point]:
+                        print(f'Point: {point}')
+                        break
+                    point_count += 1
+                point = coords[point_count]
+                if event == 'w':
+                    point = point[0], point[1] - 1
+                if event == 'a':
+                    point = point[0] - 1, point[1]
+                if event == 's':
+                    point = point[0], point[1] + 1
+                if event == 'd':
+                    point = point[0] + 1, point[1]
+                coords[point_count] = point
+                print(f'Adjusted: {coords}')
+            warped, warped_encoded = warpPerspective(feed.getFrame(True)[0], coords, dims=(width, height))
             skip_warp = True
+
+        # elif len(coords) == 4 and event in ['w', 'a', 's', 'd']:
+        #     point_count = 0
+        #
+        #     print(f'Adjusted: {adj_coords}')
+        #     warped, warped_encoded = warpPerspective(feed.getFrame(True)[0], adj_coords, dims=(width, height))
 
         # print(f'Length of Coords: {len(coords)}, Skip Warp: {skip_warp}')
         location = center((warped.shape[1], warped.shape[0]))
@@ -174,7 +196,7 @@ def scaleFrame(img, desired_height=400, desired_width=300):
     return resize(img, (width, height)), scale
 
 
-def center(input_dim, preview_dim=(600, 300)):
+def center(input_dim, preview_dim=(600, 400)):
     # print(input_dim)
     hor_dif = preview_dim[0] - input_dim[0]
     vert_dif = preview_dim[1] - input_dim[1]
